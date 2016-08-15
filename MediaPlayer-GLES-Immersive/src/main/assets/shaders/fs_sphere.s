96455b1ebf5a6eef9675071f521201d6f4819f6b
Mario Guggenberger <mg@protyposis.net>
2016-08-15 13:07:05 +0200

    Simplify shader by removing zero-vectors, one-factors and always-false if clauses

--- MediaPlayer-GLES-Immersive/src/main/assets/shaders/fs_sphere.s
+++ MediaPlayer-GLES-Immersive/src/main/assets/shaders/fs_sphere.s
@@ -17,43 +17,14 @@ varying vec2 v_TextureCoord;
 
 vec4 trace(in vec2 p)
 {
-    // parametric form of a ray: = O + tD
-    vec3 O = vec3(0.0, 0.0, 0.0); // ray origin O
     vec3 D = normalize(vec3(p, 1.0)); // ray direction D
 
-    vec3 C = vec3(0.0, 0.0, 0.0); // sphere center C
-    float r = 1.0; // sphere radius r
-
-    vec3 L = C - O;
-    float tca = dot(L, D);
-
-    if(tca < 0.0) {
-        // nothing in front of ray
-        return vec4(0.0, 0.0, 1.0, 1.0);
-    }
-
-    float d2 = dot(L, L) - (tca * tca);
-    float r2 = r * r;
-
-    if(d2 > r2) {
-        // ray misses sphere
-        return vec4(0.0, 1.0, 0.0, 1.0);
-    }
-
-    float thc = sqrt(r2 - d2);
-
-    float t0 = tca - thc;
-    float t1 = tca + thc;
-
     // calculate hit point of ray on sphere
-    vec3 Phit = O + t0 * D;
-    Phit = (rotation * vec4(Phit, 1.0)).xyz;
-    vec3 Nhit = normalize(Phit - C);
+    vec3 sp = (rotation * vec4(-D, 1.0)).xyz;
 
     // calculate texture mapping for hit point
-    vec3 sp = Phit - C; // calculate sphere point position with zero origin
     float phi = atan(sp.z, sp.x);
-    float theta = acos(sp.y / r);
+    float theta = acos(sp.y);
 
     // Spherical mapping from sphere to texture
     float u = 0.5 - (phi + PI) / TWOPI + 0.25;
