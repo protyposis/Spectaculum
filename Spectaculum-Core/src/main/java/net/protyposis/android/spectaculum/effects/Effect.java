45c4ff4655157c621df4fb28dc59d8ac0755def2
Mario Guggenberger <mg@protyposis.net>
2016-08-18 16:16:14 +0200

    Add documentation to Effect interface

--- Spectaculum-Core/src/main/java/net/protyposis/android/spectaculum/effects/Effect.java
+++ Spectaculum-Core/src/main/java/net/protyposis/android/spectaculum/effects/Effect.java
@@ -25,20 +25,50 @@ import net.protyposis.android.spectaculum.gles.Framebuffer;
 import net.protyposis.android.spectaculum.gles.Texture2D;
 
 /**
+ * Interface to shader effects for the SpectaculumView.
  * Created by Mario on 18.07.2014.
  */
 public interface Effect {
 
+    /**
+     * Callback interface for effect events.
+     */
     interface Listener {
+        /**
+         * Gets called when a parameter of the effect has changed.
+         * @param effect the effect whose parameter has changed
+         */
         void onEffectChanged(Effect effect);
     }
 
+    /**
+     * Gets the name of the effect.
+     * @return the name of the effect
+     */
     String getName();
 
+    /**
+     * Initializes the effect by loading all required resources (shaders, framebuffers, textures,
+     * subeffects, ...) and preparing it for usage. The resolution of the render pipeline respectively
+     * the texture resolution for internal processing must be supplied.
+     * @param width the texture width
+     * @param height the texture height
+     */
     void init(int width, int height);
 
+    /**
+     * Returns the initialization status of the effect.
+     * @return true if the effect is initialized and ready to use, else false
+     */
     boolean isInitialized();
 
+    /**
+     * Applies the effect to a source texture and writes it to the target framebuffer. The source
+     * texture is the input image data that the effect is applied to, and the target can be an
+     * intermediate framebuffer (for chaining to another effect) or the screen for direct output.
+     * @param source the source texture where the input image is read from
+     * @param target the target framebuffer where the result with the applied effect is written to
+     */
     void apply(Texture2D source, Framebuffer target);
 
     /**
@@ -50,11 +80,29 @@ public interface Effect {
      */
     void setParameterHandler(ParameterHandler handler);
 
+    /**
+     * Adds a parameter to the effect. Parameters can be used to parameterize parameters of the effect :)
+     * @see Parameter
+     * @param param the parameter to add
+     */
     void addParameter(Parameter param);
 
+    /**
+     * Gets a list of available parameters of the effect.
+     * @return list of effect parameters
+     */
     List<Parameter> getParameters();
 
+    /**
+     * Checks if this effect has any parameters.
+     * @return true if there are parameters attached, else false
+     */
     boolean hasParameters();
 
+    /**
+     * Sets a listener that gets called when any parameter value of the effect has changed.
+     * @see Listener#onEffectChanged(Effect)
+     * @param listener the listener to call back
+     */
     void setListener(Listener listener);
 }
