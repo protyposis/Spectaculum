28420dba064bfbbff64457eff54969bd584b5060
Mario Guggenberger <mg@protyposis.net>
2024-02-20 17:21:10 +0100

    feat(demo): avoid write permission through media store

--- Spectaculum-Demo/src/main/java/net/protyposis/android/spectaculumdemo/Utils.java
+++ Spectaculum-Demo/src/main/java/net/protyposis/android/spectaculumdemo/Utils.java
@@ -17,9 +17,14 @@
 package net.protyposis.android.spectaculumdemo;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.TextView;
@@ -29,6 +34,8 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Objects;
 
 import net.protyposis.android.spectaculum.SpectaculumView;
 
@@ -46,18 +53,6 @@ public class Utils {
         subtitleView.setEllipsize(TextUtils.TruncateAt.MIDDLE);
     }
 
-    public static boolean saveBitmapToFile(Bitmap bmp, File file) {
-        try {
-            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
-            bmp.compress(Bitmap.CompressFormat.PNG, 90, bos);
-            bos.close();
-            return true;
-        } catch (IOException e) {
-            Log.e(TAG, "failed to save frame", e);
-        }
-        return false;
-    }
-
     /**
      * An implementation of the OnFrameCapturedCallback that saves captured frames to png files.
      */
@@ -73,12 +68,29 @@ public class Utils {
 
         @Override
         public void onFrameCaptured(Bitmap bitmap) {
-            File targetFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-                    mFileNamePrefix + System.currentTimeMillis() + ".png");
-            if(Utils.saveBitmapToFile(bitmap, targetFile)) {
-                Toast.makeText(mContext, "Saved frame to " + targetFile.getPath(),
-                        Toast.LENGTH_LONG).show();
-            } else {
+            String fileName = mFileNamePrefix + System.currentTimeMillis() + ".png";
+
+            try {
+                OutputStream fos;
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    ContentResolver resolver = mContext.getContentResolver();
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, fileName);
+                    contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/png");
+                    contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES);
+                    Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+                    fos = resolver.openOutputStream(Objects.requireNonNull(imageUri));
+                } else {
+                    File targetFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), fileName);
+                    fos = new FileOutputStream(targetFile);
+                }
+
+                bitmap.compress(Bitmap.CompressFormat.PNG, 90, fos);
+                Objects.requireNonNull(fos).close();
+                Toast.makeText(mContext, "Saved to pictures directory as " + fileName, Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                Log.e(TAG, "Failed saving frame", e);
                 Toast.makeText(mContext, "Failed saving frame", Toast.LENGTH_LONG).show();
             }
         }
